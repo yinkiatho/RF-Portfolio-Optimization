ffb0f72d7fee48d81254cadb01166482ba3f3e38
yinkiatho <108441585+yinkiatho@users.noreply.github.com>
2023-11-09 15:23:58 +0800

    bugs

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ st.subheader("Model Architecture")
 st.write("Ensemble Machine Learning Model built with Random Forest Regressor and Multi-layer Perceptron regressor, stacked together with a Gradient Boosting Regressor.")
 st.write("Optimised using GridSearchCV and Walk Foward Validation Technique")
 
-st.image("model.drawio.png", use_column_width=True)
+st.image("/Misc/model.drawio.png", use_column_width=True)
 
 with st.expander("Model Code"):
     code = """for ticker in tickers:
