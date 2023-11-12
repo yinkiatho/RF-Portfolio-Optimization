a6524a091d6d759678c28599a3093421ac678a96
yinkiatho <108441585+yinkiatho@users.noreply.github.com>
2023-11-12 15:28:52 +0800

    qs

--- streamlit_app.py
+++ streamlit_app.py
@@ -306,12 +306,9 @@ with tab1:
     pdf_file_path = "./Misc/stats.pdf"
     # get pdf file
     with open(pdf_file_path, "rb") as file:
-        base64_pdf = base64.b64encode(file.read()).decode("utf-8")
+        pdf_display = file.read()
 
-    pdf_display = (
-        f'<embed src="data:application/pdf;base64,{base64_pdf}" '
-        'width="800" height="1000" type="application/pdf"></embed>'
-    )
+    
     st.markdown(pdf_display, unsafe_allow_html=True)
     
 
